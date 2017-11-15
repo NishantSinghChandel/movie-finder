51587f6c16c1e65e6bca422dc1286696896dad8e
Nishant Singh <nishant@grappus.com>
2017-11-16 01:29:45 +0530

    gitignore intialized

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 app.set('port',port);
 app.use(express.static(__dirname + '/public'));
 app.get('/[^\.]+$', function(req,res) {
-  res.set('Content-Type', 'text/html')
+  res.set('Content-Type', 'text/html)
      .sendFile(path.join(__dirname,'/public/index.html'))
 });
 
